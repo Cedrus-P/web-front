82c6b7baa5c89dc709988d3bb52b7714bc975c68
Cedrus-P <80530649+Cedrus-P@users.noreply.github.com>
2022-06-14 21:13:42 +0800

    tw first use

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<div class="bg-slate-800 rounded-xl p-8 md:p-0 m-5">
+		<img
+			class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none"
+			src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
+			alt=""
+		/>
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">
+        “这是使用 tailwindcss 来实现的一个基础的卡片区域，在实现该卡片区域的过程中，我们从来没有离开过 HTML 区域.”
+      </p>
+      <div class="font-medium ">
+        <div class="text-sky-400">CEDRUS</div>
+        <div class="text-slate-500">web前台</div>
+      </div>
+    </div>
+	</div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
