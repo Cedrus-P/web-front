8208dcabb1a929ac30176efc3ba84a2ca17bea74
Cedrus-P <80530649+Cedrus-P@users.noreply.github.com>
2022-06-19 19:06:18 +0800

    feat: slider hover

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,5 +1,8 @@
 <script setup>
 import mSvgIcon from '@/libs/svg-icon/index.vue'
+import { ref, watch, onBeforeUpdate } from 'vue'
+import { useScroll } from '@vueuse/core'
+
 // vite 构建的项目中，我们可以使用 defineProps 方法
 defineProps({
   data: {
@@ -7,23 +10,77 @@ defineProps({
     required: true
   }
 })
+
+// 滑块样式
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '70px'
+})
+
+// 选中item下标
+const currentCategoryIndex = ref(0)
+
+// 获取所有item元素
+let itemRefs = []
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+
+// 数据改变之后，DOM改变之前
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+
+// 获取ul元素
+const ulTarget = ref(null)
+// 通过 usescroll 获取响应式 scroll 距离
+const { x: ulScrollLeft } = useScroll(ulTarget)
+
+// watch监听
+watch(currentCategoryIndex, (val) => {
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  sliderStyle.value = {
+    // 滑块的位置 = ul横向滚动的位置 + 当前元素的left - ul的padding
+    transform: `translateX(${ulScrollLeft.value + left - 10}px)`,
+    width: width + 'px'
+  }
+})
+
+// item 点击事件
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
 </script>
 <template>
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul
-      class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+      ref="ulTarget"
+      class="relative flex overflow-x-auto items-center p-1 text-xs text-zinc-600 overflow-hidden"
     >
+      <!-- 滑块 -->
+      <li
+        ref="sliderTarget"
+        :style="sliderStyle"
+        class="absolute h-[25px] bg-slate-400 rounded-lg duration-200"
+      ></li>
       <!-- 汉堡按钮 -->
       <li
-        class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
+        class="z-20 fixed right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
       >
         <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
       </li>
 
       <!-- category item -->
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
         :key="item.id"
+        :ref="setItemRef"
+        @click="onItemClick(index)"
+        :class="{
+          'text-zinc-100': currentCategoryIndex === index
+        }"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
       >
         {{ item.name }}
