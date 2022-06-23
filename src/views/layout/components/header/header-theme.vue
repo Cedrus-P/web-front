7270c9e819e1acc20cec394bb4a38ecce56c4c49
Cedrus-P <80530649+Cedrus-P@users.noreply.github.com>
2022-06-23 16:27:48 +0800

    use slot

--- src/views/layout/components/header/header-theme.vue
+++ src/views/layout/components/header/header-theme.vue
@@ -1,6 +1,30 @@
 <script setup>
 import mPophover from '@/libs/pophover/index.vue'
 import mSvgIcon from '@/libs/svg-icon/index.vue'
+
+import { THEME_DARK, THEME_LIGHT, THEME_SYSTEM } from '@/constants'
+
+// 构建渲染数据源
+const themeArr = [
+  {
+    id: '0',
+    type: THEME_LIGHT,
+    icon: 'theme-light',
+    name: '极简白'
+  },
+  {
+    id: '1',
+    type: THEME_DARK,
+    icon: 'theme-dark',
+    name: '极夜黑'
+  },
+  {
+    id: '2',
+    type: THEME_SYSTEM,
+    icon: 'theme-system',
+    name: '跟随系统'
+  }
+]
 </script>
 <template>
   <div class="demo-container">
@@ -18,11 +42,11 @@ import mSvgIcon from '@/libs/svg-icon/index.vue'
           class="flex items-center p-1 cursor-pointer rounded hover:bg-slate-200/40"
         >
           <m-svg-icon
-            name="theme-light"
+            name="item.icon"
             fillClass="fill-zinc-900"
             class="w-1.5 h-1.5 mr-1"
           ></m-svg-icon>
-          <span class="text-zinc-800 text-sm">极简白</span>
+          <span class="text-zinc-800 text-sm">{{ item.name }}</span>
         </div>
       </div>
     </m-pophover>
