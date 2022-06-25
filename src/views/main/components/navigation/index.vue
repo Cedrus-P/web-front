5212c35009c5ada36fc82ee4642e9ee700c7f98d
Cedrus-P <80530649+Cedrus-P@users.noreply.github.com>
2022-06-25 21:05:00 +0800

    use vuex store

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,22 +1,14 @@
+<template>
+  <mobile-navigation-vue v-if="isMobileTerminal" />
+</template>
+
 <script setup>
 import { isMobileTerminal } from '@/utils/flexible'
 import mobileNavigationVue from './mobile/index.vue'
-import { getCategory } from '@/api/category'
-import { ref } from 'vue'
-import { ALL_CATEGORY_ITEM } from '@/constants'
+import { useStore } from 'vuex'
 
-const categoryData = ref([])
-const getCategoryData = async () => {
-  const { categorys } = await getCategory()
-  categoryData.value = categorys
-  categoryData.value.unshift(ALL_CATEGORY_ITEM)
-}
-getCategoryData()
+const store = useStore()
+// 触发 category 数据获取动作
+store.dispatch('category/useCategoryData')
+// console.log(store)
 </script>
-<template>
-  <mobile-navigation-vue
-    v-if="isMobileTerminal"
-    :data="categoryData"
-  ></mobile-navigation-vue>
-</template>
-<style lang="scss" scoped></style>
